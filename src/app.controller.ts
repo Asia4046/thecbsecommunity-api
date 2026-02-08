61a353bbe578bf348e5d586ad121386cfd413612
Asia4046 <kirito234875@gmail.com>
2026-02-08 22:55:03 +0530

    Initialize Project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
+  getHello(): {api_name: string, api_id: string, api_version: string, api_status: string, api_message: string } {
     return this.appService.getHello();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,19 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(): {
+    api_name: string;
+    api_id: string;
+    api_version: string;
+    api_status: string;
+    api_message: string;
+  } {
+    return {
+      api_name: 'The CBSE Community Questions API',
+      api_id: 'DEV_0.0.1',
+      api_version: '0.0.1-dev',
+      api_status: 'Online',
+      api_message: 'Welcome!',
+    };
   }
 }
