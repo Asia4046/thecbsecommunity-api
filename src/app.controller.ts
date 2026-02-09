bd498f8ecd90384d649d9257bc1d69c2b7096332
Asia4046 <kirito234875@gmail.com>
2026-02-09 19:19:19 +0530

    Initialize Project

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,13 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): {api_name: string, api_id: string, api_version: string, api_status: string, api_message: string } {
+  getHello(): {
+    api_name: string;
+    api_id: string;
+    api_version: string;
+    api_status: string;
+    api_message: string;
+  } {
     return this.appService.getHello();
   }
 }
